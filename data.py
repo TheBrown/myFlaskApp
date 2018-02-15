0052d11363c387c6682f0e544670a5b6d7596fb4
Saleumsack <saleumsack16@gmail.com>
2018-02-15 14:56:11 +0700

    first commit

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article One',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, cumque asperiores nobis sunt non quisquam nisi quaerat enim hic reprehenderit, vel, eum odio incidunt ullam modi. Tenetur voluptatum iure ducimus.',
+            'author' : 'Saleumsack KEOBOUALAY',
+            'create_date' : '15-2-2018'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article Two',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, cumque asperiores nobis sunt non quisquam nisi quaerat enim hic reprehenderit, vel, eum odio incidunt ullam modi. Tenetur voluptatum iure ducimus.',
+            'author' : 'Phithaksin SOMVANXAY',
+            'create_date' : '11-2-2018'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article Three',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, cumque asperiores nobis sunt non quisquam nisi quaerat enim hic reprehenderit, vel, eum odio incidunt ullam modi. Tenetur voluptatum iure ducimus.',
+            'author' : 'Khanthalath LATHXACHAK',
+            'create_date' : '8-1-2018'
+        },
+        {
+            'id' : 4,
+            'title' : 'Article Four',
+            'body' : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, cumque asperiores nobis sunt non quisquam nisi quaerat enim hic reprehenderit, vel, eum odio incidunt ullam modi. Tenetur voluptatum iure ducimus.',
+            'author' : 'Outhai SAIOUDOM',
+            'create_date' : '21-12-2017'
+        }
+    ]
+
+    return articles
